a50be4f5de172cf3d9b0782dca84690f0ce65697
moverq1337 <moverq1337@gmail.com>
2025-01-06 21:25:41 +0300

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './index.css'
 
 function App() {
 	return (
-		<div className='px-4 py-8'>
-			<LeftNav />
-			<CourseData />
+		<div>
+			<div className='absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
+			<div className='px-4 py-8'>
+				<LeftNav />
+				<CourseData />
+			</div>
 		</div>
 	)
 }
