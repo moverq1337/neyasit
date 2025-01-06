3592e02b6f67153ea386d6e816478771aad05ce6
unkabas <undercannabis10@gmail.com>
2025-01-06 21:26:42 +0300

    fix cards

--- src/components/course-card/Course-card.tsx
+++ src/components/course-card/Course-card.tsx
@@ -1,33 +1,35 @@
 import { Card, CardBody, CardFooter } from '@nextui-org/react'
+import { Play } from 'lucide-react'
 import CustomButton from '../button/CustomButton'
 
 type Props = {
 	image: React.ReactNode
-
 	title: string
-
 	description: string
-
 	price: string
-
 	className?: string
 }
 
 const CourseCard: React.FC<Props> = props => {
 	return (
 		<Card
-			className={`rounded-2xl bg-[#0D1618] shadow-[0px_0px_30px_2px_#343C25] w-96 mt-10 ${props.className}`}
+			className={`relative rounded-2xl bg-[#0D1618] shadow-[0px_0px_30px_2px_#343C25] w-96 mt-10 ${props.className}`}
 		>
 			<CardBody className='p-4'>
-				{props.image}
-				<div className='mt-4 text-white text-[20px] font-normal  leading-[17px] tracking-[0.6px]'>
+				<div className='relative'>
+					{props.image}
+					<button className='absolute inset-0 flex items-center justify-center hover:scale-125 duration-200'>
+						<Play size={50} className='text-white' />
+					</button>
+				</div>
+				<div className='mt-4 text-white text-[20px] font-normal leading-[17px] tracking-[0.6px]'>
 					{props.title}
 				</div>
-				<div className='mt-4 text-[#AAA] text-[10px] font-normal  leading-[11px] tracking-[0.3px]'>
+				<div className='mt-4 text-[#AAA] text-[10px] font-normal leading-[11px] tracking-[0.3px]'>
 					{props.description}
 				</div>
 				<div className='flex items-center justify-between mt-6'>
-					<div className='text-[#FFF]  font-normal text-[14px] leading-[17px] tracking-[0.42px]'>
+					<div className='text-[#FFF] font-normal text-[14px] leading-[17px] tracking-[0.42px]'>
 						{props.price}
 					</div>
 					<CustomButton variant='solid' color='primary'>
--- src/course-data/course-data.tsx
+++ src/course-data/course-data.tsx
@@ -1,24 +1,24 @@
-import card from '../assets/card.png'
+import card2 from '../assets/card2.png'
 import CourseCard from '../components/course-card/Course-card'
 
 const CoursesList: React.FC = () => {
 	const courses = [
 		{
-			img: <img src={card} alt='card' />,
+			img: <img src={card2} alt='card2' />,
 			title: 'Основы web-разработки',
 			description:
 				'Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
 			price: '$100',
 		},
 		{
-			img: <img src={card} alt='card' />,
+			img: <img src={card2} alt='card2' />,
 			title: 'Основы web-разработки',
 			description:
 				'Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
 			price: '$100',
 		},
 		{
-			img: <img src={card} alt='card' />,
+			img: <img src={card2} alt='card2' />,
 			title: 'Основы web-разработки',
 			description:
 				'Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
