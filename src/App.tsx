41f6f13b0ef4290d3ab2c5fd1fc6931064865c10
unkabas <undercannabis10@gmail.com>
2025-01-06 21:06:24 +0300

    add cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LeftNav from './components/LeftNav'
+import CourseData from './course-data/course-data'
 import './index.css'
 
 function App() {
 	return (
 		<div className='px-4 py-8'>
 			<LeftNav />
+			<CourseData />
 		</div>
 	)
 }
---
+++ src/course-data/course-data.tsx
@@ -0,0 +1,44 @@
+import card from '../assets/card.png'
+import CourseCard from '../components/course-card/Course-card'
+
+const CoursesList: React.FC = () => {
+	const courses = [
+		{
+			img: <img src={card} alt='card' />,
+			title: 'Основы web-разработки',
+			description:
+				'Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+			price: '$100',
+		},
+		{
+			img: <img src={card} alt='card' />,
+			title: 'Основы web-разработки',
+			description:
+				'Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+			price: '$100',
+		},
+		{
+			img: <img src={card} alt='card' />,
+			title: 'Основы web-разработки',
+			description:
+				'Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+			price: '$100',
+		},
+	]
+
+	return (
+		<div className='grid grid-cols-4 gap-56'>
+			{courses.map((el, index) => (
+				<CourseCard
+					key={index}
+					image={el.img}
+					title={el.title}
+					description={el.description}
+					price={el.price}
+				/>
+			))}
+		</div>
+	)
+}
+
+export default CoursesList
